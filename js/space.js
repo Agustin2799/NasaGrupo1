5f97f2a32025ec848b9918532670b5b5e38c147a
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2023-09-08 10:05:30 -0300

    Add files via upload

---
+++ js/space.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById("inputBuscar");
+    const valorInput = input.value;
+    const contenedor = document.getElementById("contenedor");
+    const boton = document.getElementById("btnBuscar");
+
+    function mostrarDatos(datos) {
+        datos.collection.items.forEach(item => {
+            const div = document.createElement('div');
+            div.classList.add('resultado');
+            div.innerHTML = `
+                <h1>${item.data[0].title}</h1>
+                <img src="${item.links[0].href}" alt="Descripción de la imagen">
+                <p>Descripción: ${item.data[0].description}</p>
+                <p>Fecha: ${item.data[0].date_created}</p>
+            `;
+            contenedor.appendChild(div);
+        });
+    }
+
+    boton.addEventListener("click", () => {
+        const contenedor = document.getElementById("contenedor");
+        contenedor.innerHTML = "";
+        fetch(`https://images-api.nasa.gov/search?q=${valorInput}`)
+            .then(response => response.json())
+            .then(data => {
+                mostrarDatos(data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    });
+});
